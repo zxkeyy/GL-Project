5f5063e4bbb986dffcf5a3a2c79fdacfc1656d9d
Elaziz Dhia eddine <dhiaatipaza42@gmail.com>
2024-12-03 17:09:06 +0100

    core: setup the mobile application

---
+++ mobile/babel.config.js
@@ -0,0 +1,9 @@
+export default function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
+}
