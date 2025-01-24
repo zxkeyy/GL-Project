b6cc01e93908ea02be11a8185a6193dc2ff4fbc6
zxkeyy <z_selmani@estin.dz>
2025-01-24 18:32:53 +0100

    feat: add superuser creation to build script for initial setup

--- backend/dz_delivery/build.sh
+++ backend/dz_delivery/build.sh
@@ -11,3 +11,9 @@ python manage.py collectstatic --no-input
 
 # Run migrations
 python manage.py migrate
+
+# create superuser
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=Z_selmani@estin.dz
+export DJANGO_SUPERUSER_PASSWORD=changeme123
+python manage.py createsuperuser --noinput
