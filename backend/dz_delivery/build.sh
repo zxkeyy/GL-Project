2a8fcd272056468237281c792762b955eb146c80
zxkeyy <z_selmani@estin.dz>
2025-01-24 17:37:29 +0100

    feat: remove virtual environment setup from build script

--- backend/dz_delivery/build.sh
+++ backend/dz_delivery/build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-# Python virtual environment setup
-python -m venv venv
-. venv/bin/activate
-
 # Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
