d2322e282683ba5b789482039589db9062289b2a
zxkeyy <z_selmani@estin.dz>
2025-01-26 00:53:01 +0100

    feat: enable CORS credentials and configure trusted origins for local development

--- backend/dz_delivery/dz_delivery/settings.py
+++ backend/dz_delivery/dz_delivery/settings.py
@@ -149,6 +149,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
